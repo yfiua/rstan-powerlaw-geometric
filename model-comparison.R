b1a01c4cca073c61e3f676bda1b09a265380c79f
Jun S <yfiua6@gmail.com>
2025-05-06 21:34:08 +0200

    model comparison

---
+++ model-comparison.R
@@ -0,0 +1,41 @@
+library(rstan)
+library(readr)
+library(bridgesampling)
+
+# options for rstan
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+# read data
+synthetic_data <- read_csv("synthetic_data_pl.csv")
+
+stan_data <- list(N = nrow(synthetic_data),
+                  y = synthetic_data$x,
+                  k_min = 1,
+                  k_max = max(synthetic_data$x))
+
+# read stan models
+stan_model_pl <- stan_model("discrete-powerlaw.stan")
+stan_model_exp <- stan_model("discrete-exponential.stan")
+
+# fit the models
+fit_pl <- sampling(
+  stan_model_pl,
+  data = stan_data,
+  iter = 2000,
+  chains = 4,
+  seed = 123
+)
+
+fit_exp <- sampling(
+  stan_model_exp,
+  data = stan_data,
+  iter = 2000,
+  chains = 4,
+  seed = 123
+)
+
+bayes_factor(
+  bridge_sampler(fit_pl, silent = TRUE),
+  bridge_sampler(fit_exp, silent = TRUE)
+)
